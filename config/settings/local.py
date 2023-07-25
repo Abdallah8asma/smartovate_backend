9acb3fa856e28d083cfa4eea552a2b471470938e
Akram BEN GHANEM <akrambenghanem76@gmail.com>
2023-07-25 12:08:13 +0000

    changes in the requirements

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="M614bxZxJVW7DQ7FprZZkNJgMPQSFPNwsT5SlrODh9siIoHYpIQ9vPFntrrKc0lL",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["8000-caustazagro-caustazabac-m5wek099cs5.ws-eu101.gitpod.io", "127.0.0.1"]
+ALLOWED_HOSTS = ["8000-caustazagro-caustazabac-m5wek099cs5.ws-eu101.gitpod.io", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -39,13 +39,28 @@ DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.history.HistoryPanel",
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+]
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2", "*"]
 if env("USE_DOCKER") == "yes":
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips] + ["*"]
 
 # django-extensions
 # ------------------------------------------------------------------------------
@@ -55,3 +70,12 @@ INSTALLED_APPS += ["django_extensions"]  # noqa: F405
 # Your stuff...
 # ------------------------------------------------------------------------------
 CSRF_TRUSTED_ORIGINS = ["https://8000-caustazagro-caustazabac-m5wek099cs5.ws-eu101.gitpod.io", "http://*"]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+}
