c131ddfd6563ced809b43c5d93191707c42b1896
Asma <asmaabdallah518@gmail.com>
2025-01-31 13:54:03 +0000

    infra

--- caustaza_backend_project/jobs/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from caustaza_backend_project.jobs.models import Job
-
-
-class JobsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Job
-        fields = "__all__"
